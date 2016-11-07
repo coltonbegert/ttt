542525c99a9dbdd2a77f4ce7350bd4e851a345ec
coltonbegert <coltonbegert@gmail.com>
2016-11-06 17:16:15 -0700

    handles ties

--- winning_state.py
+++ winning_state.py
@@ -2,12 +2,26 @@ def winning_state(board, player):
     # check if a state is winning for the given player
     for i in range(3):
         if all(board[:, i] == player):
-            return True
+            return 1
         if all(board[i, :] == player):
-            return True
+            return 1
     if board[1,1] == player:
         if board[0,0] == board[1,1] == board[2,2]:
-            return True
+            return 1
         if board[0,2] == board[1,1] == board[2,0]:
-            return True
-    return False
+            return 1
+
+    opponent = (player == 1 : 2 ? 1)
+    for i in range(3):
+        if all(board[:, i] == opponent):
+            return -1
+        if all(board[i, :] == opponent):
+            return -1
+    if board[1,1] == opponent:
+        if board[0,0] == board[1,1] == board[2,2]:
+            return -1
+        if board[0,2] == board[1,1] == board[2,0]:
+            return -1
+
+
+    return 0
