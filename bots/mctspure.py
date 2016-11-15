fab12ec8ae3231c0cdd4d3a5cd431198e73f83b0
Tristan <tmeleshk@ualberta.ca>
2016-11-15 12:08:31 -0700

    Using miniwins simulator with 1-move lookahead

--- bots/mctspure.py
+++ bots/mctspure.py
@@ -2,6 +2,7 @@ from bots.base_bot import BaseBot
 from math import sqrt, log
 from time import time, sleep
 from random import choice, shuffle, randint, random
+import numpy as np
 import threading
 
 class Bot(BaseBot):
@@ -11,10 +12,13 @@ class Bot(BaseBot):
 
         self.thinking_time = 15
         self.min_searches = 1000
-        self.max_searches = 10000
+        self.max_searches = 50000
 
         self.waiting = False
         self.lock = threading.Lock()
+
+        self.select_const = 1 # Const to use for UCB in MCTS.select
+        self.picking_const = -1  # Const to use when picking a move to make
 
     def start(self):
         self.last_time = time()
@@ -52,7 +56,7 @@ class Bot(BaseBot):
         player = board.player
         ## Selection
         if tree:
-            parent_score, score, move, subtree = self.get_best(tree)
+            parent_score, score, move, subtree = self.get_best(tree, self.select_const)
             board.move(*move)
             winner = self._search(board, subtree, score)
             # -- Fall through for backprop
@@ -78,9 +82,9 @@ class Bot(BaseBot):
         self._update_score(score, winner, player)
         return winner
 
-    def get_best(self, tree):
+    def get_best(self, tree, const):
         # Returns the best move in the given tree or subtree
-        return max(tree, key=self.scoring_function)
+        return max(tree, key=self.scoring_function(const))
 
     def _update_score(self, score, winner, player):
         # Updates the score for the player and winner
@@ -91,30 +95,54 @@ class Bot(BaseBot):
     def simulate(self, board):
         # Run a single simulation, returning the player number of the victor
         # or 0 for a tie
-        while board.winner is None:
-            options = board.get_valid()
-            if board.turns_left < 64:
-                # -- Take a winning move if available
-                for move in options:
-                    B = board.clone()
-                    B.move(*move)
-                    if B.winner is not None and B.winner > 0:
-                        return B.winner
-            board.move(*choice(options))
-        return board.winner
+        turns = 10
 
-    def scoring_function(self, branch):
+        # Take a 1 move win always, if available,
+        # after each player has played about 15 moves
+        # (We can start picking after the 9th move, but the chances are low
+        # that this will be a successful strategy, so don't waste our precious
+        # simulation time doing that).
+        if board.turns_left < 50:
+            for move in board.get_valid():
+                B = board.clone()
+                B.move(*move)
+                if B.winner is not None and B.winner > 0: return B.winner
+
+        while board.winner is None and turns > 0:
+            options = board.get_valid()
+            board.move(*choice(options))
+            turns -= 1
+        if board.winner is not None:
+            return board.winner
+        state = board._miniwins
+        final_state = state + np.random.randint(1,3, state.shape)*(state==0)
+        for p in [1,2]:
+            for i in range(3):
+                if (final_state[:,i] == p).all():
+                    return p
+                if (final_state[i,:] == p).all():
+                    return p
+        if final_state[0,0] == final_state[1,1] == final_state[2,2]:
+            return final_state[1,1]
+        if final_state[2,0] == final_state[1,1] == final_state[0,2]:
+            return final_state[1,1]
+        return 0
+
+    def scoring_function(self, const):
         # Prefer branches that have high confidence
         # Break ties randomly
+        return lambda branch: self.confidence(branch, const)
+
+    def score_branch(self, branch):
         return self.confidence(branch), random()
 
-    def confidence(self, branch):
+    def confidence(self, branch, c=.707):
         # UCB formula
         parent_score, score, move, subtree = branch
         if score[1] == 0 or parent_score[1] == 0: return float('inf')
 
         mean = score[0] / score[1]
-        return mean + sqrt(2*log(parent_score[1]) / score[1]) # UCB
+        return mean + c*sqrt(log(parent_score[1]) / score[1]) # UCB
 
     def request(self):
         # Ask the bot for a move
@@ -128,10 +156,18 @@ class Bot(BaseBot):
 
         print("\rOkay, I got it.")
 
-        branch = self.get_best(self.tree)
+        print("-- Potential moves --")
+        for i,branch in enumerate(sorted(self.tree, key=self.confidence, reverse=True)):
+            parent_score, score, move, subtree = branch
+            print("{:4s}".format(str(i+1)), move, "->", "{:12s}".format(str(score)), "{:.3f}".format(self.confidence(branch)))
+
+        # Pick the move that is least likely to be a bad move
+        branch = self.get_best(self.tree, self.picking_const)
         parent_score, score, move, subtree = branch
+        print()
         print("Choosing move {} with score {} and confidence {:.3f}".format(move, score, self.confidence(branch)))
         print("  Root score was {}".format(self.root_score))
+        print()
 
         assert parent_score == self.root_score, self.tree
         return move
