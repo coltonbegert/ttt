37780b31f4364316c6dcdeda78d39f80534d67ba
Tristan <tmeleshk@ualberta.ca>
2016-11-14 19:01:29 -0700

    Fixed replayer for the host

--- Host_UTTT.py
+++ Host_UTTT.py
@@ -3,16 +3,24 @@ import board
 import traceback
 
 def main():
-    B = board.Board()
-    s = server.Server(B)
-    try:
-        s.start()
-    finally:
-        print('\n########')
-        print("Shutting server down...")
-        print('########\n')
-        s.close()
+    with open('moves.dat', 'w') as f:
+        B = BoardRecorder(f)
+        s = server.Server(B)
+        try:
+            s.start()
+        finally:
+            print('\n########')
+            print("Shutting server down...")
+            print('########\n')
+            s.close()
 
+class BoardRecorder(board.Board):
+    def __init__(self, file):
+        super(BoardRecorder, self).__init__()
+        self._file = file
+    def move(self, row, col, player=None):
+        super(BoardRecorder, self).move(row, col, player)
+        self._file.write('({},{})\n'.format(row,col))
 
 if __name__ == '__main__':
     main()
--- replayer.py
+++ replayer.py
@@ -1,19 +1,30 @@
 from board import Board
+from time import sleep
 
 # Replays a game of UTTT loaded from an input file
-def replay(infile='moves.dat'):
+def replay(infile, pretty_print=False, step_time=0, show_options=False):
     B = Board()
     with open(infile) as f:
         for line in f:
+            line = line.strip()
+            if not line: continue
+            if line[0] == '#': continue
             _,sr,_,sc,*_ = line
             r = int(sr)
             c = int(sc)
 
             print("Player", B.player)
-            print(B)
-            print(B.get_valid())
+            if not pretty_print:
+                print(B)
+            else:
+                B.pprint()
+
+            if show_options: print(B.get_valid())
+
             print("played ({},{})".format(r,c))
             B.move(r,c, B.player)
+
+            sleep(step_time)
 
     print(B)
     if B.winner:
@@ -24,4 +35,4 @@ def replay(infile='moves.dat'):
     return B.winner
 
 if __name__ == '__main__':
-    replay()
+    replay('moves.dat', True, 2, False)
