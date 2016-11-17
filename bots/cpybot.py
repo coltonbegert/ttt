210afe2afd3c81b73584a2805e71f3424880a2cc
Tristan <tmeleshk@ualberta.ca>
2016-11-16 17:25:51 -0700

    Use lazy imports for fast mcts in case the module isn't compiled

--- bots/cpybot.py
+++ bots/cpybot.py
@@ -1,5 +1,4 @@
 from bots.base_bot import BaseBot
-import fast_mcts
 
 class Bot(BaseBot):
     """
@@ -8,26 +7,31 @@ class Bot(BaseBot):
     Your C code needs to #include cpybot.h
     as the last import.
     """
-
     def setup(self, *args):
+        import fast_mcts
         fast_mcts.setup(*args)
 
     def start(self):
         """ Called after the connection is made """
+        import fast_mcts
         fast_mcts.start()
 
     def stop(self):
         """ Called after the game is over """
+        import fast_mcts
         fast_mcts.stop()
 
     def on_update(self, last_player, last_move):
         """ Called after a move is made """
+        import fast_mcts
         fast_mcts.update(last_player, last_move)
 
     def update_board(self, board):
         """ Called on a board wipe signal (NOT IMPLEMENTED) """
+        import fast_mcts
         pass
 
     def request(self):
         """ Called when the bot is expected to make a turn """
+        import fast_mcts
         return fast_mcts.request()
